67e919b8de5ecd0b5b212d674eea80315a4928c6
Kovács Bence <h266039@stud.u-szeged.hu>
2024-05-05 16:46:55 +0200

    Profil linkek hozzáadása, felesleges route-ok törlése

--- routes/admin.js
+++ routes/admin.js
@@ -36,53 +36,10 @@ router.get('/cikkek/:azon', async (req, res) => {
         return;
     }
     if (!szerzo) {
-        let szerzo = "Ismeretlen";
+        szerzo = "Ismeretlen";
     }
     res.render('cikk', {"title": cikk.CIM, cikk, szerzo, user: req.user, edit: true});
 }); 
-// @deprecated - use /cikkek/:azon/edit instead
-router.get('/cikkeke/:azon', async (req, res) => {
-    const cikkek = new cikkDAO(req.conn);
-    const felhasznalok = new felhasznaloDAO(req.conn);
-    const cikk = await cikkek.getByAzon(req.params.azon);
-    if (!cikk) {
-        res.status(404).send("404 Not Found");
-        return;
-    }
-    let szerzo = (await felhasznalok.getByAzon(cikk?.SZERZOAZON))?.NEV;
-    if (!szerzo) {
-        szerzo = "Ismeretlen";
-    }
-    const kDao = new kulcsszoDAO(req.conn);
-    const kulcsszavak = await kDao.getAll(); 
-    cikk.kulcsszavak = await cikkek.getKulcsszavak(req.params.azon);
-    const hasonlocikkek = await cikkek.getHasonlo(req.params.azon);
-    res.render('cikkEdit', {"title": cikk.CIM, cikk, szerzo, user: req.user,kulcsszavak,hasonlo:hasonlocikkek, edit: true});
-});
-
-router.post('/cikkek/update/:azon', async (req, res) => {
-    const cikkek = new cikkDAO(req.conn);
-    const kulccsszavak = new kulcsszoDAO(req.conn);
-    const regiCikk = await cikkek.getByAzon(req.params.azon);
-    if (req.body.azon && req.body.cim && req.body.tartalom && req.user && (req.user.azon === regiCikk.SZERZOAZON || req.user.admin)) {
-        await cikkek.updateCikk(req.body.azon, req.body.cim, req.body.tartalom);
-        await kulccsszavak.updateKulcsszavak(req.body.azon, req.body.kulcsszavak);
-        // TODO: triggerrel növelni a módosítások számát
-        res.redirect("/admin");
-        return;
-    }
-    res.sendStatus(403);
-});
-
-router.get('/cikkeke', async (req, res) => {
-    if (!req.user?.admin) {
-        res.status(403).send('403 Forbidden');
-        return;
-    }
-    const cikk = new cikkDAO(req.conn);
-    const cikkek = await cikk.getAll();
-    res.render('list', {"title": "Cikkek", data : cikkek,user: req.user, edit: true, route: "/admin/cikkeke/"});
-});
 
 router.get('/felhasznalok', async (req, res) => {
     if (!req.user?.admin) {
@@ -94,55 +51,6 @@ router.get('/felhasznalok', async (req, res) => {
     res.render('list', {"title": "Felhasználók", data : felhasznalok,user: req.user});
 });
 
-router.get('/felhasznaloke', async (req, res) => {
-    if (!req.user?.admin) {
-        res.status(403).send('403 Forbidden');
-        return;
-    }
-    const felhasznalo = new felhasznaloDAO(req.conn);
-    const felhasznalok = await felhasznalo.getAll();
-    res.render('list', {"title": "Felhasználók", data : felhasznalok,user: req.user, edit: true, route: "/admin/felhasznaloke/"});
-});
-
-router.get('/felhasznaloke/:azon', async (req, res) => {
-    if (!req.user?.admin) {
-        res.status(403).send('403 Forbidden');
-        return;
-    }
-    const felhasznalo = new felhasznaloDAO(req.conn);
-    const felhasznalok = await felhasznalo.getAll();
-    let user = await felhasznalo.getByAzon(req.params.azon);
-    res.render('register', {"title": "Felhasználók", edit: true, route: "/admin/felhasznaloke/", user: user});
-});
-
-router.post('/felhasznaloke/:azon', async (req, res) => {
-    let azon = req.params.azon
-    let admin = 0;
-    let lektor = null;
-    if(req.body.admin){
-        admin = 1;
-    }
-    if(req.body.lektor){
-        lektor = 1;
-    }
-    let f = new felhasznaloDAO(req.conn);
-    let l = new lektorDAO(req.conn);
-    let user = await f.getByAzon(azon);
-    if(user.LEKTORAZON == null){
-        if(lektor == 1){
-            await l.insertLektor(req.body.fokozat, req.body.intezmeny, req.body.szakterulet);
-            lektor = await l.getLatestAzon();
-            lektor = lektor['MAX(AZON)'];
-        }
-    }
-    
-    
-    await f.updateFelhasznaloLektor(azon, lektor);
-    
-    await f.updateFelhasznaloAdmin(azon, admin);
-
-    res.redirect("/admin");
-});
 
 router.get('/kulcsszavak', async (req, res) => {
     if (!req.user?.admin) {
@@ -152,16 +60,6 @@ router.get('/kulcsszavak', async (req, res) => {
     const kulcsszo = new kulcsszoDAO(req.conn);
     const kulcsszavak = await kulcsszo.getAll();
     res.render('list', {"title": "Kulcsszavak", data : kulcsszavak,user: req.user});
-});
-
-router.get('/kulcsszavake', async (req, res) => {
-    if (!req.user?.admin) {
-        res.status(403).send('403 Forbidden');
-        return;
-    }
-    const kulcsszo = new kulcsszoDAO(req.conn);
-    const kulcsszavak = await kulcsszo.getAll();
-    res.render('list', {"title": "Kulcsszavak", data : kulcsszavak,user: req.user, edit: true, route: "/admin/kulcsszavake/"});
 });
 
 router.get('/lektorok', async (req, res) => {
@@ -174,16 +72,6 @@ router.get('/lektorok', async (req, res) => {
     res.render('list', {"title": "Lektorok", data : lektorok,user: req.user});
 });
 
-router.get('/lektoroke', async (req, res) => {
-    if (!req.user?.admin) {
-        res.status(403).send('403 Forbidden');
-        return;
-    }
-    const lektor = new lektorDAO(req.conn);
-    const lektorok = await lektor.getAll();
-    res.render('list', {"title": "Lektorok", data : lektorok,user: req.user,edit: true, route: "/admin/lektoroke/"});
-});
-
 router.get('/nyelvek', async (req, res) => {
     if (!req.user?.admin) {
         res.status(403).send('403 Forbidden');
@@ -192,16 +80,6 @@ router.get('/nyelvek', async (req, res) => {
     const nyelv = new nyelvDAO(req.conn);
     const nyelvek = await nyelv.getAll();
     res.render('list', {"title": "Nyelvek", data : nyelvek,user: req.user});
-});
-
-router.get('/nyelveke', async (req, res) => {
-    if (!req.user?.admin) {
-        res.status(403).send('403 Forbidden');
-        return;
-    }
-    const nyelv = new nyelvDAO(req.conn);
-    const nyelvek = await nyelv.getAll();
-    res.render('list', {"title": "Nyelvek", data : nyelvek,user: req.user,edit: true, route: "/admin/nyelveke/"});
 });
 
 router.get('/hibajelentesek', async (req, res) => {
@@ -226,17 +104,6 @@ router.get('/hibajelentesek/:azon', async (req, res) => {
     res.render('hibajelentes', {"title": "Hibajelentés: " + cikk.CIM, hibajelentes, cikk, user: req.user});
     res.render('list', {"title": "Hibabejelentések", data : hibajelentesek,user: req.user});
 });
-
-router.get('/hibajelenteseke', async (req, res) => {
-    if (!req.user?.admin) {
-        res.status(403).send('403 Forbidden');
-        return;
-    }
-    const hibajelentes = new hibajelentesDAO(req.conn);
-    const hibajelentesek = await hibajelentes.getAll();
-    res.render('list', {"title": "Hibabejelentések", data : hibajelentesek,user: req.user,edit: true, route: "/admin/hibajelenteseke/"});
-});
-
 
 router.get('/reset', async (req, res) => {
     if (!req.user?.admin) {
